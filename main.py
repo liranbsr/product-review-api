7a16c09c8f8d8a15394dfba1ce146e3db1204916
liranbsr <liran.benshimol@gmail.com>
2025-04-06 19:31:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ Format:
 }}
 """
 
-    try:
+try:
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[{"role": "user", "content": prompt}],
